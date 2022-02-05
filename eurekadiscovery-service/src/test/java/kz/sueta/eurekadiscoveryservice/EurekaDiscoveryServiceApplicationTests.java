690fcea22fc73106177087d84f864a10a9b6687a
koldassovnt <koldassovnt@gmail.com>
2022-02-05 10:31:29 +0600

    added eurekadiscovery-service

---
+++ eurekadiscovery-service/src/test/java/kz/sueta/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sueta.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
