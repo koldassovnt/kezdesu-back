093ff4e69611fce18af4dbb55c5f19726820308b
koldassovnt <koldassovnt@gmail.com>
2022-02-05 14:03:41 +0600

    push service is connected with eurekadiscovery

--- push-service/src/main/java/kz/sueta/pushservice/PushServiceApplication.java
+++ push-service/src/main/java/kz/sueta/pushservice/PushServiceApplication.java
@@ -2,8 +2,10 @@ package kz.sueta.pushservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PushServiceApplication {
 
 	public static void main(String[] args) {
