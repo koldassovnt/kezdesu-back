afaa03ac7c1a022da22f2a6b5fb2ecbe5a7defba
koldassovnt <koldassovnt@gmail.com>
2022-02-05 16:28:26 +0600

    generateClientId added to util

--- client-service/src/main/java/kz/sueta/clientservice/util/RndUtil.java
+++ client-service/src/main/java/kz/sueta/clientservice/util/RndUtil.java
@@ -2,9 +2,15 @@ package kz.sueta.clientservice.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.util.UUID;
+
 public class RndUtil {
 
     public static String getRndCodeForAuth() {
         return RandomStringUtils.randomNumeric(4);
+    }
+
+    public static String generateClientId() {
+        return UUID.randomUUID().toString();
     }
 }
