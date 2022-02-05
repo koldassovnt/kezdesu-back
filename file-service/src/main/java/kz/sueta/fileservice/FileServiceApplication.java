b5f2b6db8df18591090f76ae96bc0b84db9e2b96
koldassovnt <koldassovnt@gmail.com>
2022-02-05 14:20:59 +0600

    removed file-service

---
+++ file-service/src/main/java/kz/sueta/fileservice/FileServiceApplication.java
@@ -0,0 +1,17 @@
+package kz.sueta.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class FileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServiceApplication.class, args);
+	}
+
+}
