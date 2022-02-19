07a498a048fd5da7ed08799581cc2353a183f497
koldassovnt <koldassovnt@gmail.com>
2022-02-19 12:58:42 +0600

    added config cloud

--- client-service/src/main/java/kz/sueta/clientservice/bean/impl/SmsSendConfigReal.java
+++ client-service/src/main/java/kz/sueta/clientservice/bean/impl/SmsSendConfigReal.java
@@ -1,19 +1,25 @@
 package kz.sueta.clientservice.bean.impl;
 
 import kz.sueta.clientservice.bean.SmsSendConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
-@PropertySource({"classpath:config.properties"})
 public class SmsSendConfigReal implements SmsSendConfig {
 
-    @Value("${smsSendServiceUseFake}")
-    private boolean useFake;
+    private final Environment environment;
+
+    @Autowired
+    public SmsSendConfigReal(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public boolean useFake() {
-        return useFake;
+        String useFakeValue = environment.getProperty("smsSendServiceUseFake");
+        return Objects.equals("true", useFakeValue);
     }
 }
---
+++ config-server/src/test/java/kz/sueta/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sueta.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
