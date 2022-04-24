b770136a02c9b056ec0cecf72fc4f38746221e79
koldassovnt <koldassovnt@gmail.com>
2022-04-24 20:41:09 +0600

    event-service added logging

--- event-service/src/main/java/kz/sueta/eventservice/service_messaging/FileServiceClient.java
+++ event-service/src/main/java/kz/sueta/eventservice/service_messaging/FileServiceClient.java
@@ -5,12 +5,17 @@ import io.github.resilience4j.retry.annotation.Retry;
 import kz.sueta.eventservice.dto.request.FileCreateRequest;
 import kz.sueta.eventservice.dto.response.FileIdModel;
 import kz.sueta.eventservice.util.ServiceFallBackStatic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+
 @FeignClient(name = "file-ws")
 public interface FileServiceClient {
+
+    Logger log = LoggerFactory.getLogger(FileServiceClient.class);
 
     @PostMapping("/file/saveFile")
     @Retry(name = "file-ws")
@@ -18,9 +23,9 @@ public interface FileServiceClient {
     FileIdModel saveFile(@RequestBody FileCreateRequest fileCreateRequest);
 
     default FileIdModel getFileFallback(FileCreateRequest fileCreateRequest, Throwable throwable) {
-        System.out.println("RequestBody = " + fileCreateRequest);
-        System.out.println("Exception class=" + throwable.getClass().getName());
-        System.out.println("Exception took place: " + throwable.getMessage());
+        log.info("RequestBody = " + fileCreateRequest);
+        log.info("Exception class=" + throwable.getClass().getName());
+        log.info("Exception took place: " + throwable.getMessage());
         return FileIdModel.of(ServiceFallBackStatic.SERVICE_FALLBACK_ID);
     }
 }
